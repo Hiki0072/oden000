9911113428cca32dd23439e12a65819a2614db2c
Hiki0072 <24hs213@s.bgu.ac.jp>
2024-12-23 14:23:26 +0900

    Update app (1).js

--- app (1).js
+++ app (1).js
@@ -23,5 +23,5 @@
                 result = "マイペース";
             }
 
-            document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
+            document.getElementById('result-output').innerText = "あなたの性格は【" + result + "】です";
         }
