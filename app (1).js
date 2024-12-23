369afc62054f1cb1756cdc3f320c5b497abb0a1c
Hiki0072 <24hs213@s.bgu.ac.jp>
2024-12-23 14:12:23 +0900

    Update app (1).js

--- app (1).js
+++ app (1).js
@@ -10,17 +10,17 @@
             } else if (colour === "仰向け" && number === 2) {
                 result = "おっちょこちょい";
             } else if (colour === "うつ伏せ" && number === 0) {
-                result = "飽き性";
+                result = "おっちょこちょい";
             } else if (colour === "うつ伏せ" && number === 1) {
-                result = "目立ちたがり屋";
+                result = "マイペース";
             } else if (colour === "うつ伏せ" && number === 2) {
-                result = "ナルシスト";
+                result = "真面目";
             } else if (colour === "横向き" && number === 0) {
-                result = "努力家";
+                result = "真面目";
             } else if (colour === "横向き" && number === 1) {
-                result = "サイコパス";
+                result = "おっちょこちょい";
             } else if (colour === "横向き" && number === 2) {
-                result = "ひねくれもの";
+                result = "マイペース";
             }
 
             document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
