5022acbc5612f7f46483509d5384910069330373
Hiki0072 <24hs213@s.bgu.ac.jp>
2024-12-23 14:09:04 +0900

    Update app (1).js

--- app (1).js
+++ app (1).js
@@ -1,26 +1,26 @@
         function checkFortune() {
             var colour = document.getElementById('colour-select').value;
             var number = parseInt(document.getElementById('number-input').value, 10);
-            var result = "凶"; // デフォルトの値
+            var result = "うつ伏せ"; // デフォルトの値
 
-            if (colour === "黒" && number === 0) {
-                result = "小吉";
-            } else if (colour === "黒" && number === 1) {
-                result = "中吉";
-            } else if (colour === "黒" && number === 2) {
-                result = "凶";
-            } else if (colour === "白" && number === 0) {
-                result = "中吉";
-            } else if (colour === "白" && number === 1) {
-                result = "中吉";
-            } else if (colour === "白" && number === 2) {
-                result = "大凶";
-            } else if (colour === "オレンジ" && number === 0) {
-                result = "凶";
-            } else if (colour === "オレンジ" && number === 1) {
-                result = "大吉";
-            } else if (colour === "オレンジ" && number === 2) {
-                result = "中吉";
+            if (colour === "仰向け" && number === 0) {
+                result = "真面目";
+            } else if (colour === "仰向け" && number === 1) {
+                result = "マイペース";
+            } else if (colour === "仰向け" && number === 2) {
+                result = "おっちょこちょい";
+            } else if (colour === "うつ伏せ" && number === 0) {
+                result = "飽き性";
+            } else if (colour === "うつ伏せ" && number === 1) {
+                result = "目立ちたがり屋";
+            } else if (colour === "うつ伏せ" && number === 2) {
+                result = "ナルシスト";
+            } else if (colour === "横向き" && number === 0) {
+                result = "努力家";
+            } else if (colour === "横向き" && number === 1) {
+                result = "サイコパス";
+            } else if (colour === "横向き" && number === 2) {
+                result = "ひねくれもの";
             }
 
             document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
