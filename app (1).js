a81ff785f33a3279832978bdcbd2cb057fe1d3a6
Hiki0072 <24hs213@s.bgu.ac.jp>
2025-01-20 13:41:00 +0900

    Update app .js

--- app (1).js
+++ app (1).js
@@ -1,30 +1,27 @@
 function checkFortune() {
-    // ユーザーの選択を取得
     var colour = document.getElementById('colour-select').value;
-    var number = parseInt(document.getElementById('number-select').value, 10); // selectで選ばれた数字を取得
+    var number = parseInt(document.getElementById('number-input').value, 10);
     var result = "仰向け"; // デフォルトの値
 
-    // 寝相と数字の組み合わせに基づく性格診断結果
     if (colour === "仰向け" && number === 0) {
         result = "真面目";
     } else if (colour === "仰向け" && number === 1) {
         result = "マイペース";
     } else if (colour === "仰向け" && number === 2) {
-        result = "おっちょこちょい";
+        result = "天然";
     } else if (colour === "うつ伏せ" && number === 0) {
         result = "おっちょこちょい";
     } else if (colour === "うつ伏せ" && number === 1) {
-        result = "マイペース";
+        result = "人見知り";
     } else if (colour === "うつ伏せ" && number === 2) {
-        result = "真面目";
+        result = "短期";
     } else if (colour === "横向き" && number === 0) {
-        result = "真面目";
+        result = "頑固";
     } else if (colour === "横向き" && number === 1) {
-        result = "おっちょこちょい";
+        result = "明るい";
     } else if (colour === "横向き" && number === 2) {
-        result = "マイペース";
+        result = "自我強い";
     }
-
-    // 結果を表示
+    
     document.getElementById('result-output').innerText = "あなたの性格は【" + result + "】です";
 }
